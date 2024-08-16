da532828d1e86c6f7e627ab0f7c3e63cfabfc7af
Mahmoud Ibrahim Mohamed <jfijcc124@gmail.com>
2024-08-16 19:15:25 +0300

    first data creation by supraa

--- MahmoudIbrahim_sun&wed_5pm_C#_EF_01/Program.cs
+++ MahmoudIbrahim_sun&wed_5pm_C#_EF_01/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace MahmoudIbrahim_sun_wed_5pm_C__EF_01
+﻿using MahmoudIbrahim_sun_wed_5pm_C__EF_01.Contexts;
+using MahmoudIbrahim_sun_wed_5pm_C__EF_01.Entities;
+
+namespace MahmoudIbrahim_sun_wed_5pm_C__EF_01
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            using (var db = new StudentDBClass())
+            {
+                Department dept = new Department();
+                dept.Name = "IT";
+                dept.HireDate = DateTime.Now;
+                dept.Students = new List<Student>();
+                dept.Instructors = new List<Instructor>();
+                db.Departments.Add(dept);
+                db.SaveChanges();
+            }
+
+            using (var db = new StudentDBClass())
+            {
+                Department dept2 = new Department();
+                dept2.Name = "HR";
+                dept2.HireDate = DateTime.Now;
+                dept2.Students = new List<Student>();
+                dept2.Instructors = new List<Instructor>();
+                db.Departments.Add(dept2);
+                db.SaveChanges();
+                Console.WriteLine(db.Entry(dept2).State.ToString());
+            }
         }
     }
 }
